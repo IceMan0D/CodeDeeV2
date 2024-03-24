63bc62bc014f8c59a2174c188a5c4cee87bd083f
IceMan0D <130057695+IceMan0D@users.noreply.github.com>
2024-03-24 23:22:00 +0700

    add admin

---
+++ admin/check_permission.php
@@ -0,0 +1,15 @@
+<?php
+    session_start();
+
+    if(empty($_SESSION['sale_login']) && empty($_SESSION['admin_login'])){
+        echo '<script>alert("ท่านไม่มีสิทธิเข้าถึงหน้านี้.");</script>';
+        echo 
+        '<script>
+        setTimeout(() => {
+        },1000)
+        window.location.href = "../CodeDee/index.php";
+        </script>';
+        exit;
+    }
+    
+?>
---
+++ admin/main_admin.php
@@ -0,0 +1,29 @@
+<?php
+    $title = 'หน้าหลัก';
+?>
+<?php
+    require_once 'check_permission.php';
+    include_once 'views/partials/header.php';
+    include_once 'views/partials/navbar.php';
+?>
+
+<body>
+    welcome admin xx <?php //echo 'test' ?>
+    <button class="btn btn-primary">fd</button>
+
+
+    <?php
+        //   $sql2 = 'SELECT user.status_id, status.status_name
+        //           FROM user
+        //           INNER JOIN status 
+        //           ON user.status_id = status.status_id';
+        //   $stmt = $conn->prepare($sql2);
+        //   $stmt->execute();
+        //   $result2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        //   echo '<pre>';
+        //   var_dump($result2);
+        //   echo '</pre>';
+    ?>
+</body>
+
+</html>
