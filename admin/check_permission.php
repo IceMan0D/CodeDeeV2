a6f2c3f995bf510e8f7c2abe045d6fb9607815db
IceMan0D <jangcom2562@gmail.com>
2024-03-24 19:35:04 +0700

    add seller

--- admin/check_permission.php
+++ admin/check_permission.php
@@ -1,10 +1,12 @@
 <?php
     session_start();
 
-    if(!empty($_SESSION['sale_login'])){
-        header('location: ../saller/sale.php');
-        exit;
-    }else if(empty($_SESSION['sale_login']) && empty($_SESSION['admin_login'])){
+    // if(!empty($_SESSION['sale_login'])){
+    //     exit;
+    // }else if(empty($_SESSION['sale_login']) && empty($_SESSION['admin_login'])){
+       
+    // }
+    if(!empty($_SESSION['sale_login']) && !empty($_SESSION['admin_login'])){
         echo '<script>alert("ท่านไม่มีสิทธิเข้าถึงหน้านี้.");</script>';
         echo 
         '<script>
